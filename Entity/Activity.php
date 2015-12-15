40309b6323162c54a9127e1044852777139bab6b
Cyril Pereira <cyril.pereira@gmail.com>
2015-12-16 00:37:32 +0100

    track all activity

--- Entity/Activity.php
+++ Entity/Activity.php
@@ -13,41 +13,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Activity
 {
-    private $em;
 
-    const STATUT_UNSEEN                 = 0;
-    const STATUT_PUBLISHED              = 1;
-    const STATUT_ARCHIVED               = 2;
-
-    const TYPE_BLOG                     = "blog";
-    const TYPE_BLOG_COMMENT             = "blog_comment";
-    const TYPE_IDEA                     = "idea";
-    const TYPE_IDEA_COMMENT             = "idea_comment";
-    const TYPE_USER                     = "user";
-    const TYPE_QUESTION                 = "question";
-    const TYPE_ABUSE                    = "abuse";
-
-    const USER_IDEA_CREATED             = "USER_IDEA_CREATED";
-    const USER_IDEA_UPDATED             = "USER_IDEA_UPDATED";
-    const USER_IDEA_COMMENT             = "USER_IDEA_COMMENT";
-    const USER_IDEA_QUESTION            = "USER_IDEA_QUESTION";
-    const USER_BLOG_COMMENT             = "USER_BLOG_COMMENT";
-    const USER_ABUSE                    = "USER_ABUSE";
-
-    public $statutString = array(
-        'Unseen',
-        'Published',
-        'Archived'
-    );
-
-    private $activities = array(
-      'USER_IDEA_CREATED'             => "!user submited a new idea !idea",
-      'USER_IDEA_UPDATED'             => "!user updated an idea !idea",
-      'USER_IDEA_COMMENT'             => "!user posted a comment !comment on the idea !idea",
-      'USER_IDEA_QUESTION'            => "!user answered !answer to the question !question of the idea !idea",
-      'USER_BLOG_COMMENT'             => "!user posted   a comment !comment on the blog page !blog",
-      'USER_ABUSE'                    => "!user reported an abuse !abuse on the idea !idea",
-    );
+    const ACTION_DELETE              = "delete";
+    const ACTION_UPDATE              = "update";
+    const ACTION_CREATED             = "created";
 
     /**
      * @var integer
@@ -68,23 +37,16 @@ class Activity
     /**
      * @var string
      *
-     * @ORM\Column(name="activity", type="string", length=255, nullable=false)
-     */
-    private $activity;
-
-    /**
-     * @var string
-     *
      * @ORM\Column(name="entity_type", type="string", length=255, nullable=false)
      */
     private $entityType;
 
     /**
-     * @var integer
+     * @var string
      *
-     * @ORM\Column(name="statut", type="integer", nullable=false, options={"default" = 0})
+     * @ORM\Column(name="action", type="string", length=255, nullable=false)
      */
-    private $statut;
+    private $action;
 
     /**
      * @var \User
@@ -103,22 +65,13 @@ class Activity
      */
     private $createdAt;
 
-    /**
-     * @var \Language
-     *
-     * @ORM\ManyToOne(targetEntity="Language")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="language_id", referencedColumnName="id", onDelete="CASCADE")
-     * })
-     */
-    private $language;
-
 
     public function __construct()
     {
         $this->setCreatedAt(new \DateTime);
-        $this->setStatut(self::STATUT_UNSEEN);
     }
+
+
 
     /**
      * Get id
@@ -131,10 +84,83 @@ class Activity
     }
 
     /**
+     * Set entityId
+     *
+     * @param integer $entityId
+     *
+     * @return Activity
+     */
+    public function setEntityId($entityId)
+    {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    /**
+     * Get entityId
+     *
+     * @return integer
+     */
+    public function getEntityId()
+    {
+        return $this->entityId;
+    }
+
+    /**
+     * Set entityType
+     *
+     * @param string $entityType
+     *
+     * @return Activity
+     */
+    public function setEntityType($entityType)
+    {
+        $this->entityType = $entityType;
+
+        return $this;
+    }
+
+    /**
+     * Get entityType
+     *
+     * @return string
+     */
+    public function getEntityType()
+    {
+        return $this->entityType;
+    }
+
+    /**
+     * Set action
+     *
+     * @param string $action
+     *
+     * @return Activity
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Get action
+     *
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
      * Set createdAt
      *
      * @param \DateTime $createdAt
-     * @return Brand
+     *
+     * @return Activity
      */
     public function setCreatedAt($createdAt)
     {
@@ -154,66 +180,11 @@ class Activity
     }
 
     /**
-     * Set entityType
-     *
-     * @return Activity
-     */
-    public function setEntityType($entityType = "")
-    {
-        $this->entityType = $entityType;
-
-        return $this;
-    }
-
-    /**
-     * Get entityType
-     *
-     * @return string
-     */
-    public function getEntityType()
-    {
-        return $this->entityType;
-    }
-
-    /**
-     * Set activity
-     *
-     * @return Activity
-     */
-    public function setActivity($activity = "")
-    {
-        $this->activity = $activity;
-
-        return $this;
-    }
-
-    /**
-     * Get activity
-     *
-     * @return string
-     */
-    public function getActivity()
-    {
-        return $this->activity;
-    }
-
-    /**
-     * Set entityId
-     *
-     * @return Activity
-     */
-    public function setEntityId($entityId = false)
-    {
-        $this->entityId = $entityId;
-
-        return $this;
-    }
-
-    /**
      * Set user
      *
      * @param \Tigreboite\FunkylabBundle\Entity\User $user
-     * @return Idea
+     *
+     * @return Activity
      */
     public function setUser(\Tigreboite\FunkylabBundle\Entity\User $user = null)
     {
@@ -231,119 +202,4 @@ class Activity
     {
         return $this->user;
     }
-
-    /**
-     * Get entityId
-     *
-     * @return string
-     */
-    public function getEntityId()
-    {
-        return $this->entityId;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStringActivity()
-    {
-        return isset($this->activities[$this->getActivity()]) ? $this->activities[$this->getActivity()] : false;
-    }
-
-    public function getEntity()
-    {
-        switch($this->getEntityType())
-        {
-            case self::TYPE_IDEA:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:Idea')->find($this->getEntityId());
-                break;
-            case self::TYPE_BLOG:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:Blog')->find($this->getEntityId());
-                break;
-            case self::TYPE_USER:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:User')->find($this->getEntityId());
-                break;
-            case self::TYPE_ABUSE:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:Abuse')->find($this->getEntityId());
-                break;
-            case self::TYPE_IDEA_COMMENT:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:IdeaComment')->find($this->getEntityId());
-                break;
-            case self::TYPE_BLOG_COMMENT:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:BlogComment')->find($this->getEntityId());
-                break;
-            case self::TYPE_QUESTION:
-                return $this->em->getRepository('TigreboiteFunkylabBundle:QuestionnaireQuestionReponse')->find($this->getEntityId());
-                break;
-            default:
-                return false;
-        }
-    }
-
-    /**
-     * @param bool $em
-     * @return $this
-     */
-    public function setEntityManager($em=false)
-    {
-        if($em)
-            $this->em=$em;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->getMessage();
-    }
-
-    /**
-     * Set statut
-     *
-     * @return Activity
-     */
-    public function setStatut($statut = 0)
-    {
-        $this->statut = $statut;
-
-        return $this;
-    }
-
-    /**
-     * Get statut
-     *
-     * @return string
-     */
-    public function getStatut()
-    {
-        return $this->statut;
-    }
-
-
-
-    /**
-     * Set language
-     *
-     * @param \Tigreboite\FunkylabBundle\Entity\Language $language
-     * @return Idea
-     */
-    public function setLanguage(\Tigreboite\FunkylabBundle\Entity\Language $language = null)
-    {
-        $this->language = $language;
-
-        return $this;
-    }
-
-    /**
-     * Get language
-     *
-     * @return \Tigreboite\FunkylabBundle\Entity\Language
-     */
-    public function getLanguage()
-    {
-        return $this->language;
-    }
-
 }
--- Listener/PostPersistListener.php
+++ Listener/PostPersistListener.php
@@ -2,60 +2,67 @@
 namespace Tigreboite\FunkylabBundle\Listener;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Routing\Router;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Tigreboite\FunkylabBundle\Entity\Activity;
 
 class PostPersistListener
 {
-    private $container;
-    private $mailer;
+    private $security;
+    private $router;
+    private $user;
 
-    public function __construct(ContainerInterface $container) {
-        $this->container = $container;
-//        $this->mailer = $this->container->get('appbundle.mailer');
+    public function __construct(TokenStorage $security, Router $router) {
+
+        $this->security  = $security;
+        $this->router    = $router;
+
     }
 
     public function postUpdate(LifecycleEventArgs $args)
     {
-        $router = $this->container->get('router');
-        $entity = $args->getEntity();
-
-        $entityManager = $args->getEntityManager();
-//        dump('postUpdate');exit;
-
-//        $this->index($args);
+        $this->createActivity($args,Activity::ACTION_UPDATE);
     }
 
     public function preRemove(LifecycleEventArgs $args)
     {
-        $router = $this->container->get('router');
-        $entity = $args->getEntity();
-
-        $entityManager = $args->getEntityManager();
-//        dump('postDelete');exit;
-
-
-//        $this->index($args);
+        $this->createActivity($args,Activity::ACTION_DELETE);
     }
 
     public function postPersist(LifecycleEventArgs $args)
     {
-        $router = $this->container->get('router');
-        $entity = $args->getEntity();
+        $this->createActivity($args,Activity::ACTION_CREATED);
+    }
 
-        $entityManager = $args->getEntityManager();
-//        dump('postPersist');exit;
+    private function createActivity(LifecycleEventArgs $args,$action)
+    {
+        $entity         = $args->getEntity();
+        $entityManager  = $args->getEntityManager();
+        $this->user     = $this->getLoggedUser();
+        
+        if(!($entity instanceof Activity) &&  $this->user)
+        {
+            $activity = new Activity();
+            $activity->setUser($this->user);
+            $activity->setAction(Activity::ACTION_CREATED);
+            $activity->setEntityId($entity->getId());
+            $activity->setEntityType(get_class($entity));
+            $entityManager->persist($activity);
+            $entityManager->flush();
+        }
+    }
 
-        //TODO : Catch event
-
-        /*if ($entity instanceof Idea) {
-
-
-
-        }else
-        if ($entity instanceof IdeaComment) {
-
-
-        }*/
+    public function getLoggedUser()
+    {
+        $token = $this->security->getToken();
+        if($token)
+        {
+            $user = $token->getUser();
+            return $user && $user!='anon.' ? $user : false;
+        }else{
+            return false;
+        }
     }
 
 }
