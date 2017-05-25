a6920fa991da79e2dba3154a33d146de7a7aa263
Cyril Pereira <cyril.pereira@gmail.com>
2017-05-25 23:32:38 +0200

    Fixed FOSUserBundle user model

--- Entity/User.php
+++ Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace Tigreboite\FunkylabBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\Mapping\Annotation as Gedmo;
